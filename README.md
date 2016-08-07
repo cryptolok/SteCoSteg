b83e89a609b2f20ec9caf5b07ec04016902e15fa
cryptolok <cryptolok@protonmail.com>
2016-08-07 20:07:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# SteCoSteg
+Steath Colors Steganography
+
+SteCoSteg is the OutGuess successor and the Vernam cipher analog in steganography.
+
+Properties:
+* plausible deniability
+* tampering/jamming resistance
+* detection/rubberhose resistance
+* compatibility/portability
+* white-box
+* key recovery/uniqueness
+
+Dependencies:
+* **Python** - main code, version 2 and 3 compatible
+	- PIL - imaging library
+
+Limitations:
+* images-only as decoy
+* not optimized for big files
+* relatively big key size
+
+## How it works
+
+SteCoSteg operates images, JPEGs are used as a decoy. Then, it takes a file and hides it in the image, converting it to PNG, by giving a unique key for the file's retrieval. To unconceal a file in a PNG image, one must specify this key.
+
+The key it-self is just an encoded pixels list that contains the file.
+
+So, SteCoSteg takes a file, decomposes it by bytes, and store these bytes in pixels of an image. The security is achieved by the determination of what pixel to use as a storage.
+
+At this point, it does exactly the same thing as OutGuess, but differently.
+
+The technique is to find the pixels that are more entropic than the others, which basically means to find the least used colors and modify them.
+
+Just like Vernam cipher and OutGuess, if SteCoSteg is well used, it will be mathematically impossible to extract the concealed information. This usage requires a highly entropic image (as photo) and relatively small amount of data to store. Bigger amount of storage is possible to conceal as well, but the more data is to be concealed, less stealthy it will be, thus it is not optimized for big amount of data concealement since, the respective decoy size is needed.
+
+The variant of this technique can be used to achieve theoretic-level security with an arbitrary storage length,  by creating customized images with a specific set of colors. However, the variant will produce a result that may draw suspicion, thus unpractical for most cases, regardless the fact that it is truly white-box and can be done in an untrusted environment, without any particular tools. Also, this variant  is one of the (perhaps the) best-one that could exist in steganography because, it uses maximum data space (length) without any special compression or encoding, contrary to the other hiding techniques. The following implementation provides less secure and less universal method, but more practical without significant drawbacks.
+
+## Analysis
+
+It follows the Kerckhoffs's principle, meaning that this is white-box type steganography and all the security is not in the secrecy of the algorithm, but in the key, which is in contrast to security by obscurity, commonly used in steganography.
+
+It combats statistical analysis like LSB enhancement, pixel evaluation, Chi-Square and all sorts of histograms.
+
+Plus, it can resist LSB jamming, the message will be modified, but can be recovered without much effort. Though, if some encryption is used (like AES in CBC mode) jamming may be problematic to recover the message. Anyway, it might be logical to jam some suspicious files, but the point of SteCoSteg is to make the file as innocent as possible and jamming all the images of the entire internet is not realistic, for now.
+
+Like was mentioned, it is possible and even better to encrypt the original message before using steganography, regardless the fact that the following implementation doesn't require encryption to maintain its security, which makes it a cryptography alternative to maintain confidentiality.
+
+All this makes it very viable in extremely restricted conditions, even if the encryption will be banned and everything will be monitored every time, which makes it a valid solution for post-cypherpunk era.
+
+Just like OutGuess, this technique can be applied to any sort of data, but the current implementation is adapted only for JPEG and PNG images, although the other formats are compatible. The main disadvantage is that the implementation doesn't determine the "stealth limit" for a given decoy, but it can be visually determined.
+
+The key is recoverable with the original decoy image and the stealthed-one. However, it is designed to be a time/resource consuming process and without the original decoy image, recovery is impossible. The key is unique per image, which means that it is possible to store different files in one image with the same key.
+
+The code is commented and explained.
+
+### HowTo
+
+For Unix:
+```bash
+sudo apt-get install python python-pil || echo 'use an appropriate package manager'
+python SteCoSteg.py
+```
+For Windows, [Python](https://www.python.org/downloads/) with [PIL](http://www.pythonware.com/products/pil/). Portable versions are not known to be functional.
+
+Anyway, the PoC [website](http://cryptolok.darktech.org) is available.
+
+To stealth a file:
+1. Specify a JPEG image as a decoy
+2. Specify a file to stealth
+3. Retreive the output PNG image and the respective key
+
+To unstealth a file:
+1. Specify a PNG image as a decoy
+2. Specify an output file (not for the website)
+3. Specify a key or the original JPEG image (key only for the website)
+4. Retrieve the original file
+
+#### Notes
+
+This is just an implementation of a technology. Further improvement is possible.
+
+Mass surveillance and world control would never be possible.
+
+> "Some tourists think Amsterdam is a city of sin, but in truth it is a city of freedom. And in freedom, most people find sin."
+
+John Green, *The Fault in Our Stars*
+
---
+++ SteCoSteg.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+
+#-*- encoding:utf-8 -*-
+
+from PIL import Image
+from codecs import encode
+from base64 import b64encode, b64decode
+
+def stealth(decoy,file):
+	FILE=open(file,'rb')
+	bytes=[]
+	print('READING FILE...')
+	try:
+		byte=FILE.read(1)
+		while byte:
+# we will save the bytes
+			bytes.append(ord(byte))
+			byte=FILE.read(1)
+	finally:
+		FILE.close()
+# a color is 3 bytes, so it has to be modulo 3
+		if len(bytes)%3:
+			bytes.append(ord('\x00'))
+		if len(bytes)%3:
+			bytes.append(ord('\x00'))
+	image = Image.open(decoy,'r')
+	image=image.convert('RGB')
+	colors={}
+	print('')
+	print('ANALYZING IMAGE...')
+	width, height = image.size
+# let's make a list of used colors
+	for x in range(width):
+		for y in range(height):
+			try:
+				colors[image.getpixel((x,y))]+=1
+			except:
+				colors[image.getpixel((x,y))]=1
+# let's apply Schwartzian Transformation for sorting, in order to mitigate the Python's compatibility problem
+	colorsList = [(item[1], item) for item in colors.items()]
+	colorsList.sort()
+	colorsSorted = [value[1] for value in colorsList]
+	length=len(bytes)
+	size=len(colorsSorted)
+	if size < length:
+		print('')
+		print('!DECOY HAS NOT ENOUGH SPACE FOR THE FILE!')
+		print('')
+		exit(1)
+	substitutionColors={}
+# let's replace our data by the order of the least used colors
+	print('')
+	print('ADAPTING COLORS...')
+	for i in range(int(len(bytes)/3)):
+		substitutionColors[colorsSorted[i][0]]=[(bytes[i*3],bytes[i*3+1],bytes[i*3+2]),i]
+# even if we shuffle the pixel/color dictionary and the key will change, the modification order is still recoverable, thus the data
+	print('')
+	print('CONSTRUCTING IMAGE...')
+	key=[]
+	for i in range(len(substitutionColors)):
+		key.append((0,0))
+# -2 since, hex begins with 0x
+	maxLength=max(len(hex(width))-2,len(hex(height))-2)
+	for x in range(width):
+		for y in range(height):
+			try:
+				key[substitutionColors[image.getpixel((x,y))][1]]=(x,y)
+				image.putpixel( (x,y), substitutionColors[image.getpixel((x,y))][0])
+			except: pass
+	format='png'
+	image.save(file+'.'+format,format)
+	print('')
+	print('SAVED TO '+file+'.'+format)
+	print('')
+	print('GENERATING KEY...')
+	print('')
+# for storage optimization, we will convert the key to hex in function of the image size and then Base64
+	hexes=''
+	for coordinate in key:
+		for value in coordinate:
+# might be buggy with small images < 16 pixels
+			value=str(hex(value))[2:]
+			if len(value) < maxLength:
+				for i in range(int(maxLength-len(value))):
+					value='0'+value
+			hexes+=value
+	try:
+# Python 2
+		string=''
+		for i in range(int(len(hexes)/2)):
+			string+=chr(int(hexes[i*2]+hexes[i*2+1],16))
+		print('KEY : '+b64encode(string))
+	except:
+# Python 3
+		string='b\''
+		for i in range(int(len(hexes)/2)):
+			string+='\\x'+hexes[i*2]+hexes[i*2+1]
+		string+='\''
+		string=eval(string)
+		print('KEY : '+str(b64encode(string))[2:-1])
+	print('')
+
+def unstealth(decoy,file):
+	key=input('KEY (or recovery JPEG image) : ')
+	print('')
+	wasFile=False
+	if len(key)>4092:
+		key=input('THE KEY IS TOO LONG, SPECIFY A FILE WITH THAT KEY : ')
+		print('')
+		FILE=open(key,'r')
+		key=''
+		for line in FILE:
+			key+=line
+		FILE.close()
+		wasFile=True
+	image = Image.open(decoy,'r')
+	FILE=open(file,'wb')
+	width, height = image.size
+	maxLength=max(len(hex(width))-2,len(hex(height))-2)
+	coordinates=[]
+# all we need, is to get the modified pixels in good order
+	try:
+		if not wasFile:
+			recovery = Image.open(key,'r')
+			recovery=recovery.convert('RGB')
+			width2, height2 = recovery.size
+			if width != width2 or height != height2:
+				print('')
+				print('!WRONG IMAGE SIZE!')
+				print('')
+				exit(1)
+			colors={}
+			colors2={}
+			print('RECOVERING KEY... may take long')
+			print('')
+# let's make a list of used colors for each image
+			for x in range(width):
+				for y in range(height):
+					try:
+						colors[image.getpixel((x,y))]+=1
+						colors2[recovery.getpixel((x,y))]+=1
+					except:
+						colors[image.getpixel((x,y))]=1
+						colors2[recovery.getpixel((x,y))]=1
+# let's apply Schwartzian Transformation for sorting, in order to mitigate the Python's compatibility problem and make a difference between both lists by storing and appending them to coordinates in the right order
+			colorsList = [(item[1], item) for item in colors.items()]
+			colorsList.sort()
+			colorsSorted = [value[1] for value in colorsList]
+			colors2List = [(item[1], item) for item in colors2.items()]
+			colors2List.sort()
+			colors2Sorted = [value[1] for value in colors2List]
+			difference=list(set(colorsSorted)-set(colors2Sorted))
+			for value in range(int(len(difference))):
+				difference[value]=difference[value][0]
+			for pixel in colorsSorted:
+				if pixel[0] in difference:
+					for x in range(width):
+						for y in range(height):
+							if image.getpixel((x,y)) == pixel[0]:
+								coordinates.append((x,y))
+	except:
+# Base64 decoding
+		try:
+# Python 2
+			key=b64decode(key).encode('hex')
+		except:
+# Python 3
+			key=str(encode(b64decode(key),'hex'))[2:-1]
+		for i in range(int(len(key)/maxLength/2)):
+			coordinates.append((int(key[i*maxLength*2]+key[i*maxLength*2+1]+key[i*maxLength*2+2],16),int(key[i*maxLength*2+3]+key[i*maxLength*2+4]+key[i*maxLength*2+5],16)))
+	print('CONSTRUCTING FILE...')
+	print('')
+	for coordinate in coordinates:
+# because, tuplet and bytes difference
+		try:
+# Python 2
+			FILE.write(chr(image.getpixel(coordinate)[0]))
+			FILE.write(chr(image.getpixel(coordinate)[1]))
+			FILE.write(chr(image.getpixel(coordinate)[2]))
+		except:
+# Python 3
+			FILE.write(bytes([image.getpixel(coordinate)[0]]))
+			FILE.write(bytes([image.getpixel(coordinate)[1]]))
+			FILE.write(bytes([image.getpixel(coordinate)[2]]))
+	image.close()
+	FILE.close()
+	print('SAVED TO '+file)
+	print('')
+
+# some style
+print('')
+print('  	                 |         ')
+print('  	                / \        ')
+print('  	               //V\\\       ')
+print('  	              / \|/ \      ')
+print('  	             /=/ v \=\     ')
+print('  	            /         \    ')
+print('  	           /           \   ')
+print('  	          /             \  ')
+print('  	         /   /|     |\   \ ')
+print('  	        /   / \\\   // \   \\')
+print('  	        \  /   \\\ //   \  /')
+print('  	         \/   / \ / \   \/ ')
+print('  	             / / V \ \     ')
+print('  	            |_/     \_|    ')
+print('')
+print('   _____ _        _____      _____ _             ')
+print('  / ____| |      / ____|    / ____| |            ')
+print(' | (___ | |_ ___| |     ___| (___ | |_ ___  __ _ ')
+print('  \___ \| __/ _ \ |    / _ \\\___ \| __/ _ \/ _` |')
+print('  ____) | ||  __/ |___| (_) |___) | ||  __/ (_| |')
+print(' |_____/ \__\___|\_____\___/_____/ \__\___|\__, |')
+print('                                            __/ |')
+print('                                           |___/ ')
+print('')
+print('           Stealth Colors Steganography')
+print('')
+
+# since, input functions varie in Python 2 and 3, we have to find the good-one
+try:
+	input=raw_input
+except NameError: pass
+
+decoy=input('INPUT IMAGE (JPEG to stealth and PNG to unstealth): ')
+# GIF is possible, but can be buggy for recovery
+print('')
+file=input('FILE (input to stealth and output to unstealth): ')
+print('')
+image=Image.open(decoy,'r')
+format=image.format
+format=format.lower()
+if format == 'png':
+	unstealth(decoy,file)
+elif format == 'jpeg':
+	stealth(decoy,file)
+else:
+	print('!WRONG IMAGE FORMAT!')
+	print('')
+	exit(1)
